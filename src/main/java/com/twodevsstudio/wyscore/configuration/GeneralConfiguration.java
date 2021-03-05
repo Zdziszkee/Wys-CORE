2f6910f2aa1d5edf07f61c465c09e8c9329d4702
Zdziszkee <52966846+Zdziszkee@users.noreply.github.com>
2021-03-05 19:36:31 +0100

    - rename

--- src/main/java/com/twodevsstudio/wyscore/configuration/GeneralConfiguration.java
+++ src/main/java/com/twodevsstudio/wyscore/configuration/GeneralConfiguration.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 @Getter
 @Configuration("generalConfiguration.json")
 public class GeneralConfiguration extends Config {
-    private String mongoURI = "mongodb+srv://zdziszkee:feedforafrica@cluster0.acoet.gcp.mongodb.net/RPG?retryWrites=true&w=majority";
+    private String mongoURI = "your mongo uri";
 
 }
